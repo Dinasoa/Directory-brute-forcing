8d9196b1cc2482a61c41ff4b600a6ed9cf2fd15b
dinasoa <hei.dinasoa@gmail.com>
2024-03-18 09:46:40 +0300

    chore: compute batch_size based on wordlist length and number of cpu

--- brute_forcing/extract_routes.py
+++ brute_forcing/extract_routes.py
@@ -1,9 +1,9 @@
-import multiprocessing
-
 import requests
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
 from brute_forcing.check_routes import check_route
+import multiprocessing
+import math
 
 
 def extract_routes(dir_list, base_url):
@@ -14,37 +14,38 @@ def extract_routes(dir_list, base_url):
     with open(dir_list, 'rb') as file:
         lines = file.readlines()
 
+    # Calculate batch size for each thread based on length of the wordlist and number of cpu
+    total_words = len(lines)
+    batch_size_per_thread = math.ceil(total_words / cpu_count)
+
     # Creating a ThreadPoolExecutor with max_workers set to CPU count
-        with ThreadPoolExecutor(max_workers=cpu_count) as executor:
-            """
-            List to store futures( result of an asynchronous 
-            or parallel operation that is either currently running or has already been completed.)
-            for each batch of tasks
-            """
-            futures = []
-            # Defining the batch_size to process
-            batch_size = 10
+    with ThreadPoolExecutor(max_workers=cpu_count) as executor:
+        """
+           List to store futures( result of parallel operation that is either 
+           currently running or has already been completed.)
+           for each batch of tasks
+        """
+        futures = []
 
-            # Iterating through lines in batches
-            for i in range(0, len(lines), batch_size):
-                # Extracting a batch of lines
-                batch = lines[i:i + batch_size]
-                batch_futures = []
+        # Iterating through lines in batches
+        for i in range(0, total_words, batch_size_per_thread):
+            # Extracting a batch of lines
+            batch = lines[i:i + batch_size_per_thread]
+            batch_futures = []
 
-                for line in batch:
-                    # Decoding line and removing trailing whitespaces
-                    word = line.decode('utf-8').strip()
-                    print(f"Checking path: /{word}")
-                    # Submitting task to executor
-                    batch_futures.append(executor.submit(check_route, base_url, word))
+            for line in batch:
+                # Decoding line and removing trailing whitespaces
+                word = line.decode('utf-8').strip()
+                print(f"Checking path: /{word}")
+                # Submitting task to executor
+                batch_futures.append(executor.submit(check_route, base_url, word))
 
-                # Adding batch futures to the list of all futures
-                futures.append(batch_futures)
+            futures.append(batch_futures)
 
-            for batch_futures in futures:
-                # Iterating through futures in the current batch as they complete
-                for future in concurrent.futures.as_completed(batch_futures):
-                    # Getting the result of the completed future
-                    result = future.result()
-                    if result:
-                        print(f"{result}: exists")
+        for batch_futures in futures:
+            # Iterating through futures in the current batch as they complete
+            for future in concurrent.futures.as_completed(batch_futures):
+                # Getting the result of the completed future
+                result = future.result()
+                if result:
+                    print(f"{result}: exists")
